c161ddbb9417d730359237d819d5dd6afcde5e23
Eduardo Faria <eduardofaria.rj@gmail.com>
2024-01-29 23:30:57 -0300

    Feat: Mongoose filtering options.

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -32,10 +32,23 @@ async function createCourse() {
 
 async function getCourses() {
   try {
-    const courses = await Course.find({ author: "Mosh", isPublished: true })
-      .limit(10)
+    const pageNumber = 2;
+    const pageSize = 10;
+    // Ex: /api/courses?pageNumber=2&pageSize=10
+
+    const courses = await Course.find({ author: /^Mosh/ }) // Starts with "Mosh" with Regular Expressions (/pattern/)
+      // Ends with Hamedani: /Hamedani$/ or for NON Case sensitive /Hamedani$/i (add "i" after the "/")
+      // Contains Mosh: /.*Mosh.*/ "."(zero) or "*"(more) characters before or after Mosh
+      //.find().or([{ author: "Mosh" }, { isPublished: true }]) //find results that match this author OR publish status. An array o objects to filter
+      //.find({ author: "Mosh", isPublished: true })
+      //.find({ price: { $in: [10, 15, 20] } }) //.find({ price: 10 }) -> .find({ price: { $gt: 10, $lte: 20 } })
+      //.and([{}]) // using OR || AND you can let .find() without any arguments.
+      .skip((pageNumber - 1) * pageSize) //Pagination, means that page starts after page 1
+      .limit(pageSize)
+      //.limit(10)
       .sort({ name: 1 }) // 1: ascending / -1: descending
       .select({ name: 1, tags: 1 });
+    //.count(); // Number of matches found !!!Cannot be used with .select()!!!
     console.log(courses);
   } catch (error) {
     console.log(error.message);
@@ -44,3 +57,19 @@ async function getCourses() {
 
 getCourses();
 //createCourse();
+
+/*
+# MongoDB Comparison Operators
+eq (equal)
+ne (not equal)
+gt (greater)
+gte (greater than or equal to)
+lt (less than)
+lte (less than or equal to)
+in 
+nin (not in)
+
+# MongoDB Logical Operators
+or
+and
+*/
